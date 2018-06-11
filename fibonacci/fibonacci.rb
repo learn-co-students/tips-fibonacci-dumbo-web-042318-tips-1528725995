c569ec906a90f43a1117b229c1eedeea1350d70d
jessebillard <jessebillard@mac.com>
2018-06-11 10:24:20 -0400

    attempted the fibonacci

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -2,8 +2,14 @@ require 'pry-byebug'
 # Pry is included in case you'd like to run this file and test it,
 # but you may remove it if you like.
 
+
+
+## no gooooooooddd
 def fibonacci(n)
-  # your code here :)
+  fibonacci_array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+  sliced_fib = fibonacci_array.slice(0, n)
+  last_two = sliced_fib.slice(-2)
+  last_two[0] + last_two[1]
 end
 
 Pry.start
