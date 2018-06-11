a4f95755eb74236b20930022f797e6cedffa2b49
travisdock <travisdock@gmail.com>
2018-06-11 10:23:47 -0400

    finished

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -3,7 +3,18 @@ require 'pry-byebug'
 # but you may remove it if you like.
 
 def fibonacci(n)
-  # your code here :)
+  starting_array = [0, 1]
+  if n > 0
+    (n-1).times do
+      next_num = starting_array[-1] + starting_array[-2]
+      starting_array.push(next_num)
+    end
+    return starting_array[-1]
+  elsif n == 0
+    return 0
+  else
+    return "not a valid input"
+  end
 end
 
 Pry.start
